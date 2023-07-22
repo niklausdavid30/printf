c995c261a8a18755c91b8af4d9015e5fe5bdef4c
Peplinn <100541154+Peplinn@users.noreply.github.com>
2023-07-22 16:17:57 +0100

    Update print_string.c
    Added print_char

---
+++ print_string.c
@@ -0,0 +1,13 @@
+#include <main.h>
+
+/**
+* print_char - Prints characters
+* @argms: Variadic list of arguments
+*/
+
+int print_char(va_list argms)
+{
+  char value = va_arg(listin, int);
+  _putchar(value);
+  return (1);
+}
