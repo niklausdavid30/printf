71db8463842dbdf8810f73734a084d7613333232
Peplinn <100541154+Peplinn@users.noreply.github.com>
2023-07-22 16:32:02 +0100

    Added print_string function

--- print_string.c
+++ print_string.c
@@ -11,3 +11,17 @@ int print_char(va_list argms)
   _putchar(value);
   return (1);
 }
+
+/**
+* print_string - Prints strings
+* @argms: Variadic list of arguments
+*/
+
+int print_string(va_list argms)
+{
+  int i;
+  char *s = va_arg(argms, char *)
+  for (i = 0; s[i] != '\0'; i++)
+      _putchar(s[i]);
+  return (i);
+}
