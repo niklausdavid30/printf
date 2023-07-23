a4722c40bfaa360bf6be2e9ea1f52633205ba5ea
David Ani <100693342+niklausdavid30@users.noreply.github.com>
2023-07-23 20:48:40 +0100

    Update print_number.c
    corrected args to argms

--- print_number.c
+++ print_number.c
@@ -5,19 +5,19 @@
 * @argms: Variadic list of arguments
 */
 
-int print_integer(va_list args)
+int print_integer(va_list argms)
 {
 	int n;
 
-	n = print_number(args);
+	n = print_number(argms);
 	return (n);
 
 }
 
-int print_unsigned(va_list args)
+int print_unsigned(va_list argms)
 {
 	int n;
 
-	n = print_unsigned_number(args);
+	n = print_unsigned_number(argms, unsigned int);
 	return (n);
 }
