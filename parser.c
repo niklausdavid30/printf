b088ea7a5acb734a12320974c39f43b001e66642
Peplinn <100541154+Peplinn@users.noreply.github.com>
2023-07-24 21:51:31 +0100

    Converted tabs to indents.

--- parser.c
+++ parser.c
@@ -11,31 +11,31 @@ int parser (const char *format, FunctionList printFunctions, va_list argms)
 {
 	int i = 0, j, length = 0;
 
-  while (format[i])
-  {
-    if (format[i] == '%' && format[i+1] != '%' && format[i] != '\0')
-    {
-      int specifier_check = 0;
+	while (format[i])
+	{
+		if (format[i] == '%' && format[i+1] != '%' && format[i] != '\0')
+		{
+			int specifier_check = 0;
 
-      for (j = 0; j < 9; j++)
-      {
-        if (format[i+1] == printFunctions[j].specifier)
-        {
-          specifier_check = 1;
-          length += printFunctions.pf(j);
-          j++;
-          break;
-        }
-      }
-      if (!specifier_check)
-      {
-        length += _putchar('%');
-        i++;
-      }
-      else if (format[i] == '%' && format[i+1] = '%')
-      {
-        length += _putchar('%');
-        i++;
+			for (j = 0; j < 9; j++)
+			{
+				if (format[i+1] == printFunctions[j].specifier)
+				{
+					specifier_check = 1;
+					length += printFunctions.pf(j);
+					j++;
+					break;
+				}
+			}
+			if (!specifier_check)
+			{
+				length += _putchar('%');
+				i++;
+			}
+			else if (format[i] == '%' && format[i+1] = '%')
+			{
+				length += _putchar('%');
+				i++;
 			}
 			else if (format[0] == '%' && format[1] == '\0')
 			{
