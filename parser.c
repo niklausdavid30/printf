298e548f9ba008257cef6de84c054d94a3f457ec
Peplinn <100541154+Peplinn@users.noreply.github.com>
2023-07-25 23:26:51 +0100

    Corrected condition for space after percent sign.

--- parser.c
+++ parser.c
@@ -31,6 +31,7 @@ int parser(const char *format, FunctionList printFunctions[], va_list argms)
 			if (!specifier_check)
 			{
 				length += _putchar('%');
+				length += _putchar(format[i + 1]);
 				i++;
 			}
 		}
