d809b6207108170fb8bcc716d44934b5f016163f
Peplinn <100541154+Peplinn@users.noreply.github.com>
2023-07-26 03:55:12 +0100

    Corrected print_string

--- print_string.c
+++ print_string.c
@@ -25,20 +25,11 @@ int print_string(va_list argms)
 
 	s = va_arg(argms, char *);
 	if (s == NULL)
-	{
 		s = "(null)";
-		len = _strlen(s);
-		for (i = 0; i < len; i++)
-			_putchar(s[i]);
-		return (len);
-	}
-	else
-	{
-		len = _strlen(s);
-		for (i = 0; i < len; i++)
-			_putchar(s[i]);
-		return (len);
-	}
+	len = _strlen(s);
+	for (i = 0; i < len; i++)
+		_putchar(s[i]);
+	return (len);
 }
 
 /**
