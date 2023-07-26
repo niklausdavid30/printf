61cb8d469ebf3b580d58a567027d8489e79d408b
David Ani <100693342+niklausdavid30@users.noreply.github.com>
2023-07-26 12:07:44 +0100

    added print_customString

--- print_customString.c
+++ print_customString.c
@@ -6,40 +6,34 @@
  * @argms: Variadic list of arguments
  * Return: Number of printed chars
  */
+
 int print_custom_string(va_list argms)
 {
-    int length = 0;
-    char *str = va_arg(argms, char*);
-    char *hex_rep;
+	int length = 0;
+	char *str = va_arg(argms, char*);
+	char *hex_rep;
 
-    if (str == NULL)
-    {
-        length += write(1, "(null)", 6);
-    }
-    else
-    {
-        for (; *str; str++)
-        {
-            if ((*str > 0 && *str < 32) || *str >= 127)
-            {
-                length += write(1, "\\x", 2);
-                hex_rep = "0123456789ABCDEF";
-                length += write(1, &hex_rep[(*str >> 4) & 0x0F], 1);
-                length += write(1, &hex_rep[*str & 0x0F], 1);
-            }
-            else
-            {
-                length += write(1, str, 1);
-            }
-        }
-    }
+	if (str == NULL)
+	{
+		length += write(1, "(null)", 6);
+	}
+	else
+	{
+		for (; *str; str++)
+		{
+			if ((*str > 0 && *str < 32) || *str >= 127)
+			{
+				length += write(1, "\\x", 2);
+				hex_rep = "0123456789ABCDEF";
+				length += write(1, &hex_rep[(*str >> 4) & 0x0F], 1);
+				length += write(1, &hex_rep[*str & 0x0F], 1);
+			}
+			else
+			{
+				length += write(1, str, 1);
+			}
+		}
+	}
 
-    return (length);
+	return length;
 }
-
-
-
-
-
-
-
